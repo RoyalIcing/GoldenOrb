7b5bac2aff0ed24c8596a7f6f473e173a1e1de1a
Patrick Smith <pgwsmith@gmail.com>
2025-04-27 01:09:02 +1000

    Clean up

--- lib/css.ex
+++ lib/css.ex
@@ -5,19 +5,12 @@ end
 
 defimpl GoldenOrb.CSS, for: Any do
   def text_css(%module{} = struct) do
-    # wat = Orb.to_wat(@for)
     wat = Orb.to_wat(module)
     {:ok, store} = Wasmex.Store.new()
     {:ok, wasm_module} = Wasmex.Module.compile(store, wat)
 
-    # struct_keys = Map.keys(struct) |> Enum.map(&Atom.to_string/1)
     struct_stringly =
       struct |> Map.from_struct() |> Map.new(fn {key, value} -> {Atom.to_string(key), value} end)
-
-    # for {global_name, {:global, %{type: type, mutability: :var}}} <-
-    #       Wasmex.Module.exports(wasm_module) do
-    #   IO.puts("Global: #{inspect(global_name)} (#{inspect(type)})")
-    # end
 
     {:ok, pid} = Wasmex.start_link(%{store: store, module: wasm_module})
     {:ok, memory} = Wasmex.memory(pid)
@@ -30,7 +23,6 @@ defimpl GoldenOrb.CSS, for: Any do
     for {global_name, {:global, %{type: type, mutability: :var}}} <-
           Wasmex.Module.exports(wasm_module) do
       initial_value = Map.get(struct_stringly, global_name)
-      IO.puts("Global: #{inspect(global_name)} (#{inspect(type)}) #{inspect(initial_value)})")
 
       case initial_value do
         false ->
@@ -44,8 +36,6 @@ defimpl GoldenOrb.CSS, for: Any do
 
         atom when is_atom(atom) ->
           {:ok, [value]} = call_function.("#{global_name}_#{atom}", [])
-          # IO.inspect("#{global_name}_#{atom}")
-          IO.inspect(value)
           Wasmex.Instance.set_global_value(store, instance, global_name, value)
       end
     end
