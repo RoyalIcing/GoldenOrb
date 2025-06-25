f4b0dcc36f29bfdcba9e9582f8b7e825c80a4623
Patrick Smith <pgwsmith@gmail.com>
2025-06-25 18:59:43 +1000

    Remove unused hello/0 func

--- lib/golden_orb.ex
+++ lib/golden_orb.ex
@@ -3,19 +3,6 @@ defmodule GoldenOrb do
   Documentation for `GoldenOrb`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GoldenOrb.hello()
-      :coming_soon
-
-  """
-  def hello do
-    :coming_soon
-  end
-
   def html(conn, layout, fragments) do
     GoldenOrb.Plug.send_html(conn, layout, fragments)
   end
--- test/golden_orb_test.exs
+++ test/golden_orb_test.exs
@@ -1,8 +1,4 @@
 defmodule GoldenOrbTest do
   use ExUnit.Case
   doctest GoldenOrb
-
-  test "greets the world" do
-    assert GoldenOrb.hello() == :coming_soon
-  end
 end
